4d002edc9ca5980354674207182358c97af04112
omarhurani <omarhurani@gmail.com>
2021-10-21 22:12:04 +0300

    Add 2D sized drawable

---
+++ lib/src/controllers/drawables/sized2ddrawable.dart
@@ -0,0 +1,59 @@
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+
+import 'object_drawable.dart';
+
+/// A drawable that explicitly defines its size in two dimensions (width and height).
+abstract class Sized2DDrawable extends ObjectDrawable{
+
+  /// The size of the drawable.
+  final Size size;
+
+  /// Padding of drawable.
+  ///
+  /// This padding is added to the size in the [getSize] method.
+  /// Implementing classes can set this value to increase/decrease the size.
+  final EdgeInsets padding;
+
+
+  /// Creates a new [Sized2DDrawable] with the given [size] and [painting].
+  const Sized2DDrawable({
+    required this.size,
+    this.padding = EdgeInsets.zero,
+    required Offset position,
+    double rotationAngle = 0,
+    double scale = 1,
+    Set<ObjectDrawableAssist> assists = const <ObjectDrawableAssist>{},
+    Map<ObjectDrawableAssist, Paint> assistPaints = const <ObjectDrawableAssist, Paint>{},
+    bool hidden = false,
+  }) : super(
+      position: position,
+      rotationAngle: rotationAngle,
+      scale: scale,
+      assists: assists,
+      assistPaints: assistPaints,
+      hidden: hidden
+  );
+
+  /// Creates a copy of this but with the given fields replaced with the new values.
+  @override
+  Sized2DDrawable copyWith({
+    bool? hidden,
+    Set<ObjectDrawableAssist>? assists,
+    Offset? position,
+    double? rotation,
+    double? scale,
+    Size? size,
+    EdgeInsets? padding,
+  });
+
+  /// Calculates the size of the rendered object.
+  @override
+  Size getSize({double minWidth = 0.0, double maxWidth = double.infinity}) {
+    return Size(
+      (size.width + padding.horizontal).clamp(0, double.infinity),
+      (size.height + padding.vertical).clamp(0, double.infinity),
+    );
+  }
+}
