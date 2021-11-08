9626c9f7e5d74e8635cae20ed595f5f0a989b63d
omarhurani <omarhurani@gmail.com>
2021-11-08 22:57:48 +0200

    Add rectangle drawable

---
+++ lib/src/controllers/drawables/shape/rectangle_drawable.dart
@@ -0,0 +1,110 @@
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+
+import '../object_drawable.dart';
+import 'shape_drawable.dart';
+import '../sized2ddrawable.dart';
+
+/// A drawable of a rectangle with a radius.
+class RectangleDrawable extends Sized2DDrawable implements ShapeDrawable{
+
+  /// The paint to be used for the line drawable.
+  @override
+  Paint paint;
+
+  /// The border radius of the rectangle.
+  /// The default value is a circular radius of 5 on all corners.
+  BorderRadius borderRadius;
+
+  /// Creates a new [RectangleDrawable] with the given [size], [paint] and [borderRadius].
+  RectangleDrawable({
+    Paint? paint,
+    required Size size,
+    required Offset position,
+    double rotationAngle = 0,
+    double scale = 1,
+    Set<ObjectDrawableAssist> assists = const <ObjectDrawableAssist>{},
+    Map<ObjectDrawableAssist, Paint> assistPaints = const <ObjectDrawableAssist, Paint>{},
+    bool hidden = false,
+    this.borderRadius = const BorderRadius.all(const Radius.circular(5)),
+  }) : this.paint = paint ?? ShapeDrawable.defaultPaint,
+        super(
+          size: size,
+          position: position,
+          rotationAngle: rotationAngle,
+          scale: scale,
+          assists: assists,
+          assistPaints: assistPaints,
+          hidden: hidden
+      );
+
+  /// Draws the arrow on the provided [canvas] of size [size].
+  @override
+  void drawObject(Canvas canvas, Size size) {
+    final drawingSize = this.size * scale;
+    canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromCenter(center: position, width: drawingSize.width, height: drawingSize.height),
+        topLeft: borderRadius.topLeft,
+        topRight: borderRadius.topRight,
+        bottomLeft: borderRadius.bottomLeft,
+        bottomRight: borderRadius.bottomRight,
+      ),
+      paint
+    );
+  }
+
+  /// Creates a copy of this but with the given fields replaced with the new values.
+  @override
+  RectangleDrawable copyWith({
+    bool? hidden,
+    Set<ObjectDrawableAssist>? assists,
+    Offset? position,
+    double? rotation,
+    double? scale,
+    Size? size,
+    Paint? paint,
+  }){
+    return RectangleDrawable(
+      hidden: hidden ?? this.hidden,
+      assists: assists ?? this.assists,
+      position: position ?? this.position,
+      rotationAngle: rotation ?? this.rotationAngle,
+      scale: scale ?? this.scale,
+      size: size ?? this.size,
+      paint: paint ?? this.paint,
+    );
+  }
+
+  /// Calculates the size of the rendered object.
+  @override
+  Size getSize({double minWidth = 0.0, double maxWidth = double.infinity}) {
+    final size = super.getSize();
+    return Size(
+        size.width,
+        size.height
+    );
+  }
+
+  /// Compares two [RectangleDrawable]s for equality.
+  @override
+  bool operator ==(Object other) {
+    return other is RectangleDrawable &&
+        super == other &&
+        other.paint == paint &&
+        other.size == size;
+  }
+
+  @override
+  int get hashCode => hashValues(
+      hidden,
+      hashList(assists),
+      hashList(assistPaints.entries),
+      position,
+      rotationAngle,
+      scale,
+      paint,
+      size);
+
+}
--- lib/src/controllers/drawables/shape/shape_drawables.dart
+++ lib/src/controllers/drawables/shape/shape_drawables.dart
@@ -1,3 +1,4 @@
 export 'shape_drawable.dart';
 export 'line_drawable.dart';
 export 'arrow_drawable.dart';
+export 'rectangle_drawable.dart';
