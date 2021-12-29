e57e7e5526e90562fd04659aef7a8d611be3cc7a
omarhurani <omarhurani@gmail.com>
2021-12-30 01:09:41 +0200

    Add grouped drawable

---
+++ lib/src/controllers/drawables/grouped_drawable.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:collection/collection.dart';
+
+import 'drawable.dart';
+
+class GroupedDrawable extends Drawable{
+
+  /// The list of drawables in this group.
+  final List<Drawable> drawables;
+
+  /// Creates a new [GroupedDrawable] with the list of [drawables].
+  GroupedDrawable({
+    required List<Drawable> drawables,
+    bool hidden = false,
+  }) : drawables = List.unmodifiable(drawables), super(hidden: hidden);
+
+  /// Draw all the drawables in the group on [canvas] of [size].
+  @override
+  void draw(Canvas canvas, Size size) {
+    for(final drawable in drawables)
+      drawable.draw(canvas, size);
+  }
+
+  /// Compares two [GroupedDrawable]s for equality.
+  @override
+  bool operator ==(Object other) {
+    return other is GroupedDrawable &&
+        super == other &&
+        ListEquality().equals(drawables, other.drawables);
+  }
+
+  @override
+  int get hashCode => hashValues(hidden, hashList(drawables));
+}
