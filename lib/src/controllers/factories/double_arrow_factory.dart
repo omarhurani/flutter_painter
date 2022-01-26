1a6957b1f1b794d86520028ecca978153d0621fe
omarhurani <omarhurani@gmail.com>
2022-01-26 18:08:06 +0200

    Fix documentation

--- lib/src/controllers/factories/double_arrow_factory.dart
+++ lib/src/controllers/factories/double_arrow_factory.dart
@@ -3,12 +3,12 @@ import 'dart:ui';
 import '../drawables/shape/double_arrow_drawable.dart';
 import 'shape_factory.dart';
 
-/// A [DoubleArrowFactory] factory.
+/// A [DoubleArrowDrawable] factory.
 class DoubleArrowFactory extends ShapeFactory<DoubleArrowDrawable> {
   /// The size of the arrow head to be used in created [DoubleArrowDrawable]s.
   double? arrowHeadSize;
 
-  /// Creates an instance of [DoubleArrowFactory] with the given [arrowHeadSize].
+  /// Creates an instance of [DoubleArrowDrawable] with the given [arrowHeadSize].
   DoubleArrowFactory({this.arrowHeadSize}) : super();
 
   /// Creates and returns a [DoubleArrowDrawable] with length of 0 and the passed [position] and [paint].
