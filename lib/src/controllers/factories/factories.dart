d405237230857ed02423d4fa2db84889d910cf16
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-10-15 17:47:21 +0200

    feat: added new drawble models to exports

--- lib/src/controllers/factories/factories.dart
+++ lib/src/controllers/factories/factories.dart
@@ -1,6 +1,7 @@
-export 'shape_factory.dart';
-export 'line_factory.dart';
 export 'arrow_factory.dart';
+export 'double_arrow_factory.dart';
+export 'line_factory.dart';
+export 'node_polygon_factory.dart';
 export 'oval_factory.dart';
 export 'rectangle_factory.dart';
-export 'double_arrow_factory.dart';
+export 'shape_factory.dart';
